e2b89405bc3c31ac2e1e14b62d75aa65c2fbf386
jerickmalungcut <jerickmalungcut@yahoo.com>
2023-12-05 00:58:58 +0800

    added mapping of data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
 import Employees from "./components/Employees";
 import FullName from "./components/FullName";
 import InputValue from "./components/InputValue";
+import data from "./constants";
 
 function App() {
   const showEmployee = true;
 
   return (
     <div className="bg-slate-800 text-white h-screen w-full flex flex-col justify-center items-center">
-      <h1>Hello World!</h1>
+      <h1 className="text-3xl mb-8">Hello World!</h1>
       {showEmployee ? (
-        <>
-          <Employees name="Jerick Malungcut" job="Web Dev" />
-          <Employees />
-          <Employees />
-          <Employees />
-        </>
+        <div className="flex flex-wrap gap-4 justify-center items-center mb-8">
+          {data.map((item, index) => (
+            <div
+              key={index}
+              className="bg-white text-slate-800 min-w-[350px] min-h-[100px] flex justify-center items-center gap-6 rounded-xl shadow-lg py-4"
+            >
+              <div className="w-[100px] h-[100px] rounded-full">
+                <img src={item.img} className="rounded-full bg-cover" />
+              </div>
+              <div>
+                <h1 className="text-lg font-bold">{item.name} </h1>
+                <p className="text-sm">
+                  {item.job},{item.age}
+                </p>
+
+                <button>Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
       ) : (
         <h1>No employees found</h1>
       )}
---
+++ src/constants/index.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    name: "Jerick Malungcut",
+    job: "Web Developer",
+    age: "31",
+    img: "https://scontent.fmnl8-3.fna.fbcdn.net/v/t39.30808-6/313006102_6191180560896855_7976075147131716967_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeHPzFVkT-X59kZlxuvdYsJZ1oYg_ZYAJqHWhiD9lgAmoaCxXfreR5Gp_GUJAnDr_JWKdWhoY_uZp9rr43vWOT4G&_nc_ohc=NEmotBtRgWEAX9aW85Z&_nc_ht=scontent.fmnl8-3.fna&oh=00_AfCcMmA1SiegRiVgkbHlmYdklcpQ3eMKoh_iOkQcIXxemA&oe=6573239C",
+  },
+  {
+    name: "Darwin Malungcut",
+    job: "Flight Attendant",
+    age: "26",
+    img: "https://scontent.fmnl8-3.fna.fbcdn.net/v/t39.30808-6/313006102_6191180560896855_7976075147131716967_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeHPzFVkT-X59kZlxuvdYsJZ1oYg_ZYAJqHWhiD9lgAmoaCxXfreR5Gp_GUJAnDr_JWKdWhoY_uZp9rr43vWOT4G&_nc_ohc=NEmotBtRgWEAX9aW85Z&_nc_ht=scontent.fmnl8-3.fna&oh=00_AfCcMmA1SiegRiVgkbHlmYdklcpQ3eMKoh_iOkQcIXxemA&oe=6573239C",
+  },
+  {
+    name: "Glenn Malungcut",
+    job: "Chef",
+    age: "33",
+    img: "https://scontent.fmnl8-3.fna.fbcdn.net/v/t39.30808-6/313006102_6191180560896855_7976075147131716967_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeHPzFVkT-X59kZlxuvdYsJZ1oYg_ZYAJqHWhiD9lgAmoaCxXfreR5Gp_GUJAnDr_JWKdWhoY_uZp9rr43vWOT4G&_nc_ohc=NEmotBtRgWEAX9aW85Z&_nc_ht=scontent.fmnl8-3.fna&oh=00_AfCcMmA1SiegRiVgkbHlmYdklcpQ3eMKoh_iOkQcIXxemA&oe=6573239C",
+  },
+  {
+    name: "Rachelle Malungcut",
+    job: "Entrepreneur",
+    age: "35",
+    img: "https://scontent.fmnl8-3.fna.fbcdn.net/v/t39.30808-6/313006102_6191180560896855_7976075147131716967_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeHPzFVkT-X59kZlxuvdYsJZ1oYg_ZYAJqHWhiD9lgAmoaCxXfreR5Gp_GUJAnDr_JWKdWhoY_uZp9rr43vWOT4G&_nc_ohc=NEmotBtRgWEAX9aW85Z&_nc_ht=scontent.fmnl8-3.fna&oh=00_AfCcMmA1SiegRiVgkbHlmYdklcpQ3eMKoh_iOkQcIXxemA&oe=6573239C",
+  },
+  {
+    name: "Bob Malungcut",
+    job: "Supervisor",
+    age: "64",
+    img: "https://scontent.fmnl8-3.fna.fbcdn.net/v/t39.30808-6/313006102_6191180560896855_7976075147131716967_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeHPzFVkT-X59kZlxuvdYsJZ1oYg_ZYAJqHWhiD9lgAmoaCxXfreR5Gp_GUJAnDr_JWKdWhoY_uZp9rr43vWOT4G&_nc_ohc=NEmotBtRgWEAX9aW85Z&_nc_ht=scontent.fmnl8-3.fna&oh=00_AfCcMmA1SiegRiVgkbHlmYdklcpQ3eMKoh_iOkQcIXxemA&oe=6573239C",
+  },
+];
+
+export default data;
